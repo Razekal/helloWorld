601e8f6a6842dc15b87535ca83bcfeb1a6311705
razekal <william.schuy@pcc.edu>
2021-10-22 16:43:33 -0700

    added main method that prints hello world

--- src/pcc/helloWorld/helloWorld.java
+++ src/pcc/helloWorld/helloWorld.java
@@ -1,4 +1,11 @@
 package pcc.helloWorld;
 
+/**
+ * Created by William Schuy on 10/22/21
+ */
+
 public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello, World!");
+    }
 }
